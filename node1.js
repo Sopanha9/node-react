a0126299fdc6cb251a161ad0f135e7a0bf823fb4
John <lolznha72@gmail.com>
2025-11-19 08:28:33 +0700

    day1

---
+++ node1.js
@@ -0,0 +1,16 @@
+// normal funcs
+// function Testing(){
+//     console.log("Testing function called");
+// }
+
+
+// big fat arrow
+
+// const Testing = () => {
+//     console.log("Testing function called");
+// } 
+
+const Testing = (a=10) => {
+    console.log(a);
+}
+Testing(20);
